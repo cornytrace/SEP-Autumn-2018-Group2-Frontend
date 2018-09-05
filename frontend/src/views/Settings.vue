ab6386157516ff1ddafb14368c6331dbd1851f2c
Tim Honcoop <info@timhoncoop.nl>
2018-09-05 12:07:21 +0200

    Settings update with examples

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -2,19 +2,45 @@
   <div class="animated fadeIn">
     <b-row>
       <b-col sm="8" lg="6">
-        <b-card no-body header="Settings" class="bg">
-          <b-card-body class="pb-0">
-            <b-card no-body class="bg-primary">
-              <b-card-header>
-                Settings 1
-              </b-card-header>
-              <b-card-body class="pb-0">
-                <p>Data settings 1</p>
-              </b-card-body>
-            </b-card>
-          </b-card-body>
+        <b-card header="Settings" class="bg">
+
+          <!-- Checkbox example -->
+          <b-form-checkbox id="checkbox1" v-model="status" value="accepted" unchecked-value="not_accepted">
+            I accept the terms and use
+          </b-form-checkbox>
+
+          <!-- Multiple checkboxes -->
+          <b-form-group label="Stacked  checkboxes">
+            <b-form-checkbox-group stacked v-model="selected" name="flavour2" :options="options">
+            </b-form-checkbox-group>
+          </b-form-group>
+
         </b-card>
       </b-col>
     </b-row>
   </div>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        selected: [], // Must be an array reference!
+        options: [{
+            text: 'Value 1',
+            value: '1'
+          },
+          {
+            text: 'Value 2',
+            value: '2'
+          },
+          {
+            text: 'Value 3',
+            value: '3'
+          },
+        ]
+      }
+    }
+  }
+
+</script>
