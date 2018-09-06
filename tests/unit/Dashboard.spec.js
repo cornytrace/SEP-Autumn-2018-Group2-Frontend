fa346bf1b570dee8184cc9a0491dcbb0862378b0
Marten Kenbeek <marten.knbk@gmail.com>
2018-09-06 10:11:27 +0200

    Updated test suite and .travis.yml.

--- tests/unit/Dashboard.spec.js
+++ tests/unit/Dashboard.spec.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import { shallowMount, } from '@vue/test-utils'
+import { shallowMount, mount, } from '@vue/test-utils'
 import BootstrapVue from 'bootstrap-vue'
 import Dashboard from '@/views/Dashboard'
 
@@ -26,7 +26,7 @@ describe('Dashboard.vue', () => {
     expect(wrapper.is(Dashboard)).toBe(true)
   })
   it('should render correct content', () => {
-    const wrapper = shallowMount(Dashboard)
-    expect(wrapper.find('#traffic').text()).toMatch('Traffic')
+    const wrapper = mount(Dashboard)
+    expect(wrapper.html()).toContain('<div>Coursera Courses</div>')
   })
 })
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   lintOnSave: false,
   runtimeCompiler: true,
+  baseUrl: process.env.NODE_ENV === 'production'
+  ? '/SEP-Autumn-2018-Group2-Frontend/'
+  : '/',
 }
