8a17104a143eadba9ca6c543f9a47d0a9e40d96d
Tim Honcoop <t.i.m_h@hotmail.com>
2018-09-10 14:18:06 +0200

    DASH-IT

--- src/containers/DefaultContainer.vue
+++ src/containers/DefaultContainer.vue
@@ -3,7 +3,7 @@
     <AppHeader fixed>
       <SidebarToggler class="d-lg-none" display="md" mobile />
       <b-link class="navbar-brand" to="/">
-        <h4>EIT Dashboard</h4>
+        <h4>DASH-IT</h4>
       </b-link>
       <SidebarToggler class="d-md-down-none" display="lg" />
       <b-form-select class="header-select" v-model="selectedPlatform" :options="platformOptions"></b-form-select>
