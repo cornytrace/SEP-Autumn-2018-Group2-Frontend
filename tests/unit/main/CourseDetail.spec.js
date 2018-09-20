cef42430dec7eb2b5fd7ac682f3cd9145085de98
Peter9000222 <p.g.r.m.j.gerards@student.tue.nl>
2018-09-20 16:18:06 +0200

    Test case course detail fixed

--- tests/unit/main/CourseDetail.spec.js
+++ tests/unit/main/CourseDetail.spec.js
@@ -1,23 +1,49 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
 import {
   shallowMount,
+  mount,
   createLocalVue
 } from '@vue/test-utils'
-import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import CourseDetail from '@/views/platforms/coursera/CourseDetail'
 
-const localVue = createLocalVue()
-localVue.use(VueRouter)
-const router = new VueRouter()
+import mockUtils from '../mockUtils'
+import router from '@/router'
 
+const localVue = createLocalVue()
 localVue.use(BootstrapVue)
+localVue.use(Vuex)
 
 describe('CourseDetail.vue', () => {
-  it('is Vue instance', () => {
-    const wrapper = shallowMount(CourseDetail, {
+  mockUtils.initUtils()
+  let store
+
+  beforeEach(function () {
+    store = mockUtils.mockStore()
+  })
+
+  let mountComponent = () => {
+    return mount(CourseDetail, {
       localVue,
+      store,
       router,
+      stubs: ['LineGraph',
+        'ScatterGraph',
+        'BarGraph',
+        'DoughnutGraph',
+        'PolarAreaGraph',
+        'RadarGraph',
+      ],
     })
+  }
+
+  it('is Vue instance', () => {
+    const wrapper = mountComponent()
     expect(wrapper.isVueInstance()).toBe(true)
+  })
+  it('is Register', () => {
+    const wrapper = mountComponent()
+    expect(wrapper.is(CourseDetail)).toBe(true)
   })
 })
