405669199e5efec4664fc2501749b406a43d996e
Peter9000222 <p.g.r.m.j.gerards@student.tue.nl>
2018-09-20 10:39:56 +0200

    WIP: teacher view no api

--- src/views/platforms/coursera/CourseDetail.vue
+++ src/views/platforms/coursera/CourseDetail.vue
@@ -1,87 +1,141 @@
 <template>
   <div class="animated fadeIn">
+    <!--First row-->
     <b-row>
-      <b-col sm="6" md="8">
-        <b-card header="Automata System Validation">
-          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.</p>
-          <b-form-group
-            label="Select a quiz:"
-            label-for="select-quiz"
-            :label-cols="3"
-            :horizontal="true">
-            <b-form-select id="select-quiz"
-              :plain="true"
-              :options="['Automata to model behaviour','Basic behavioural equivalences', 'More behavioural equivalences']"
-              value="More behavioural equivalences">
-            </b-form-select>
-          </b-form-group>
-          <b-form-group
-            label="Select a quiz version:"
-            label-for="select-quiz-version"
-            :label-cols="3"
-            :horizontal="true">
-            <b-form-select id="select-quiz-version"
-              :plain="true"
-              :options="['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']"
-              value="13">
-            </b-form-select>
-          </b-form-group>
-        </b-card>
-      </b-col>
-      <b-col cols="12" sm="6" lg="3">
+      <b-col xs="12" sm="12" md="12" lg="6">
         <b-row>
-          <b-col sm="6" md="12">
+          <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
-              <b-card-body class="p-0 clearfix">
-                <i class="fa fa-star bg-primary p-4 font-2xl mr-3 float-left"></i>
-                <div class="h5 text-primary mb-0 pt-3">0.63</div>
-                <div class="text-muted text-uppercase font-weight-bold font-xs">Average grade</div>
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-users icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="enrolled-learners">920</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Enrolled learners</div>
               </b-card-body>
             </b-card>
           </b-col>
-          <b-col cols="12" sm="6" lg="12">
+          <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
-              <b-card-body class="p-0 clearfix">
-                <i class="fa fa-user bg-primary p-4 font-2xl mr-3 float-left"></i>
-                <div class="h5 text-primary mb-0 pt-3">107</div>
-                <div class="text-muted text-uppercase font-weight-bold font-xs">Unique learners</div>
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-user-times icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="leaving-learners">107</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Leaving learners</div>
               </b-card-body>
             </b-card>
           </b-col>
-          <b-col cols="12" sm="6" lg="12">
+        </b-row>
+        <b-row>
+          <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
-              <b-card-body class="p-0 clearfix">
-                <i class="fa fa-clipboard bg-primary p-4 font-2xl mr-3 float-left"></i>
-                <div class="h5 text-primary mb-0 pt-3">920</div>
-                <div class="text-muted text-uppercase font-weight-bold font-xs">Total responses</div>
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-check icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="finished-learners">437</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Finished learners</div>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <b-col sm="12" md="6" lg="6">
+            <b-card :no-body="true">
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-video-camera icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="no-videos">32</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Videos</div>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col sm="12" md="6" lg="6">
+            <b-card :no-body="true">
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-question icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="no-quizzes">16</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Quizzes</div>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <b-col sm="12" md="6" lg="6">
+            <b-card :no-body="true">
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-pencil icon-color p-4 font-2xl mr-0 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="no-assignments">3</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Assignments</div>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col sm="12" md="6" lg="6">
+            <b-card :no-body="true">
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-th-list icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="no-modules">8</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Modules</div>
+              </b-card-body>
+            </b-card>
+          </b-col>
+          <b-col sm="12" md="6" lg="6">
+            <b-card :no-body="true">
+              <b-card-body class="p-0 clearfix align-data mr-3">
+                <i class="fa fa-calendar icon-color p-4 font-2xl mr-3 float-left"></i>
+                <div class="h5 text-color mb-0 pt-3" id="no-cohorts">3</div>
+                <div class="text-muted text-uppercase font-weight-bold text-font-size">Cohorts</div>
               </b-card-body>
             </b-card>
           </b-col>
         </b-row>
       </b-col>
-    </b-row><!--/.row-->
+      <b-col xs="12" sm="12" md="12" lg="6">
+        <b-card id="dist-eval" header="Distribution of evaluation rating">
+          <div class="chart-wrapper" >
+            <bar-example chartId="chart-line-02" />
+          </div>
+        </b-card>
+      </b-col>
+    </b-row>
+    <!--Second row-->
     <b-row>
       <b-col md="6">
-        <b-card header="Grade distribution">
+        <b-card id="progr-fin" header="Progression of finished learners">
           <div class="chart-wrapper">
-            <line-example chartId="chart-line-01"/>
+            <line-example chartId="chart-bar-01"/>
           </div>
         </b-card>
       </b-col>
       <b-col md="6">
-        <b-card header="P-value v. RIT-score">
+        <b-card id="leav-per-mod" header="Leaving learners per module">
           <div class="chart-wrapper">
-            <scatter-example chartId="chart-scatter-01"/>
+            <doughnut-example chartId="chart-doughnut-01"/>
           </div>
         </b-card>
       </b-col>
-    </b-row><!--/.row-->
+    </b-row>
+    <!--Thirth row-->
+    <b-row>
+      <b-col md="6">
+        <b-card id="avg-time-in-course" header="Average time spend in course by learners">
+          <div class="chart-wrapper">
+            <bar-example chartId="chart-scatter-01"/>
+          </div>
+        </b-card>
+      </b-col>
+      <b-col md="6">
+        <b-card id="avg-time-in-mod" header="Average time spend per module by learners">
+          <div class="chart-wrapper">
+            <bar-example chartId="chart-polar-01"/>
+          </div>
+        </b-card>
+      </b-col> 
+    </b-row>
   </div>
 </template>
 
 <script>
 import LineExample from "@/views/charts/LineExample";
 import ScatterExample from "@/views/charts/ScatterExample";
+import BarExample from "@/views/charts/BarExample";
+import DoughnutExample from "@/views/charts/DoughnutExample";
+import PolarAreaExample from "@/views/charts/PolarAreaExample";
+import RadarExample from "@/views/charts/RadarExample";
 
 export default {
   name: "CourseDetail",
@@ -89,12 +143,18 @@ export default {
     return {
       show: true,
       course: this.$route.params.courseid,
+      lineGraphData: [0.6, 0.6, 1.55, 5.5, 0.4, 0.35, 1.0, 1.4, 1.6, 0.5],
+      lineGraphLabel: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     };
   },
   components: {
     LineExample,
     ScatterExample,
-  },
+    BarExample,
+    DoughnutExample,
+    PolarAreaExample,
+    RadarExample
+  }
 };
 </script>
 
@@ -109,5 +169,22 @@ export default {
 .fade-leave-to {
   transform: translateX(10px);
   opacity: 0;
+}
+
+.align-data {
+  text-align: right;
+}
+
+.text-font-size {
+  font-size: 10px;
+}
+
+.icon-color {
+  color: #ffffff;
+  background-color: #00a9d4;
+}
+
+.text-color {
+  color: #00a9d4;
 }
 </style>
