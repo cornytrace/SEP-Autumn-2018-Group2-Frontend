35f8aff2e333145c882ef7cac22e7d947a8cd58d
Anne Brouwers <a.s.brouwers@student.tue.nl>
2018-09-20 15:47:25 +0200

    Fixed course details icons

--- src/views/platforms/coursera/CourseDetail.vue
+++ src/views/platforms/coursera/CourseDetail.vue
@@ -7,7 +7,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
-                <i class="fa fa-users icon-color p-4 font-2xl mr-3 float-left"></i>
+                <span class="iconsquare">
+                  <i class="fa fa-users icon-color p-4 font-2xl mr-3"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="enrolled-learners">{{ enrolledStudents }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Enrolled learners</div>
               </b-card-body>
@@ -16,7 +18,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-user-times icon-color p-4 font-2xl mr-3 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="leaving-learners">{{ leavingLearners }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Leaving learners</div>
               </b-card-body>
@@ -27,7 +31,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-check icon-color p-4 font-2xl mr-3 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="finished-learners">{{ finishedLearners }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Finished learners</div>
               </b-card-body>
@@ -36,7 +42,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-video-camera icon-color p-4 font-2xl mr-3 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="no-videos">{{ numberOfVideos }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Videos</div>
               </b-card-body>
@@ -47,7 +55,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-question icon-color p-4 font-2xl mr-3 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="no-quizzes">{{ numberOfQuizzes }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Quizzes</div>
               </b-card-body>
@@ -56,7 +66,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-pencil icon-color p-4 font-2xl mr-0 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="no-assignments">{{ numberOfAssignments }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Assignments</div>
               </b-card-body>
@@ -67,7 +79,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-th-list icon-color p-4 font-2xl mr-3 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="no-modules">{{ numberOfModules }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Modules</div>
               </b-card-body>
@@ -76,7 +90,9 @@
           <b-col sm="12" md="6" lg="6">
             <b-card :no-body="true">
               <b-card-body class="p-0 clearfix align-data mr-3">
+                <span class="iconsquare">
                 <i class="fa fa-calendar icon-color p-4 font-2xl mr-3 float-left"></i>
+                </span>
                 <div class="h5 text-color mb-0 pt-3" id="no-cohorts">{{ numberOfCohorts }}</div>
                 <div class="text-muted text-uppercase font-weight-bold text-font-size">Cohorts</div>
               </b-card-body>
@@ -163,50 +179,50 @@ export default {
         {
           label: "Distribution of evaluation rating",
           backgroundColor: "#1d97d9",
-          data: [18, 6, 3, 56, 4, 25, 10, 9, 33, 3]
-        }
+          data: [18, 6, 3, 56, 4, 25, 10, 9, 33, 3,],
+        },
       ],
-      distEvalRateLabels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      distEvalRateLabels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,],
 
       // data progression of finished learners
       progFinLearData: [
         {
           label: "Progression of finished learners",
           backgroundColor: hexToRgba("#00D8FF", 90),
-          data: [1, 6, 3, 5, 4, 5, 10, 9, 6, 3]
-        }
+          data: [1, 6, 3, 5, 4, 5, 10, 9, 6, 3,],
+        },
       ],
-      progFinLearLabels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+      progFinLearLabels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,],
 
       // data leaving learners per module
       leavLearModData: [
         {
           label: "Leaving learners per module",
-          backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-          data: [80, 120, 30, 60]
-        }
+          backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16",],
+          data: [80, 120, 30, 60,],
+        },
       ],
-      leavLearModLabels: ["Module A", " Module B", "Module C", "Module D"],
+      leavLearModLabels: ["Module A", " Module B", "Module C", "Module D",],
 
       // data average time spend in a course by learners
       avgTimeCourData: [
         {
           label: "Average time spend in course by learners",
           backgroundColor: "#f879d9",
-          data: [38, 6, 5, 56, 44, 25]
-        }
+          data: [38, 6, 5, 56, 44, 25,],
+        },
       ],
-      avgTimeCourLabels: [1, 4, 5, 7, 13, 25],
+      avgTimeCourLabels: [1, 4, 5, 7, 13, 25,],
 
       // data average time spend per module by learners
       avgTimeModData: [
         {
           label: "Average timr spend per module by learners",
           backgroundColor: "#f8d979",
-          data: [28, 11, 30, 56]
-        }
+          data: [28, 11, 30, 56,],
+        },
       ],
-      AvgTimeModLabels: [1, 2, 3, 4]
+      AvgTimeModLabels: [1, 2, 3, 4,],
 
       /*
        * QDT member analytics 
@@ -220,8 +236,8 @@ export default {
     BarGraph,
     DoughnutGraph,
     PolarAreaGraph,
-    RadarGraph
-  }
+    RadarGraph,
+  },
 };
 </script>
 
@@ -247,11 +263,19 @@ export default {
 }
 
 .icon-color {
+  width: 100%;
   color: #ffffff;
-  background-color: #00a9d4;
 }
 
 .text-color {
   color: #00a9d4;
+}
+
+.iconsquare {
+  float: left;
+  background-color: #00a9d4;
+  height: 100%;
+  width: 75px;
+  text-align: center;
 }
 </style>
