d3ebffa97215c7aa0cc448d5b4de22cae0b03c10
Tim Honcoop <t.i.m_h@hotmail.com>
2018-09-27 15:56:22 +0200

    Removed another log

--- src/views/pages/ResetReferrer.vue
+++ src/views/pages/ResetReferrer.vue
@@ -5,7 +5,6 @@
 <script>
 export default {
   beforeMount: function() {
-    console.log(this.$route.params.token);
     this.$store.commit("setResetToken", this.$route.params.token);
     this.$store.commit("setResetId", this.$route.params.id);
     this.$router.push("/pages/resetpassword");
