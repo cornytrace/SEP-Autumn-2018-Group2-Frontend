ca3dadba87ae212c7c94b0c84a40d91eb0ddfbfc
Tim Honcoop <t.i.m_h@hotmail.com>
2018-10-12 14:15:35 +0200

    Started with quiz details Test cases quiz page Tests Navigation test cases added Fix Fixed test cases Implemented quiz details

--- src/views/platforms/coursera/Quizzes.vue
+++ src/views/platforms/coursera/Quizzes.vue
@@ -1,44 +1,81 @@
 <template>
   <div class="animated fadeIn">
-    <b-row>
-      <b-col sm="4" lg="4" v-for="quiz of quizzes" :key="quiz.name">
-        <b-card no-body class="bg">
-          <b-card-header>
-            <router-link :to="link + '/' + quiz.id">{{ quiz.name }}</router-link>
-          </b-card-header>
-          <b-card-body class="pb-0">
-            <b-card no-body class="bg-primary">
-              <b-card-header>
-                {{ quiz.name }} stat
-              </b-card-header>
-              <b-card-body class="pb-0">
-                <p>Data {{ quiz.name }} stat</p>
-              </b-card-body>
-            </b-card>
-          </b-card-body>
-        </b-card>
-      </b-col>
-    </b-row>
+    <div class="stats-container" v-if="!isLoading">
+      <b-row>
+        <b-col sm="4" lg="4" v-for="quiz of quizzes" :key="quiz.name">
+          <b-card no-body class="bg quiz-card">
+            <b-card-header>
+              <router-link :to="link + '/' + quiz.base_id + '-' + quiz.version">{{ quiz.name }}</router-link>
+            </b-card-header>
+            <b-card-body class="pb-0">
+              <ul>
+                <li><span class="li-title">Version:</span> {{ quiz.version }}</li>
+                <li><span class="li-title">Type:</span> {{ quiz.type }}</li>
+                <li><span class="li-title">Passing ratio:</span> {{ quiz.passing_fraction }}</li>
+              </ul>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </div>
+    <div class="loading-content" v-if="isLoading">
+      <h2>{{loadingText}}</h2>
+    </div>
   </div>
 </template>
 
 <script>
-import settings from "@/settings";
+import strings from "@/strings";
+import util from "@/util";
 
 export default {
   name: "Quizzes",
   data: function() {
     return {
-      link: "",
       quizzes: [],
+      courseId: "",
+      loadingText: strings.loading,
+      isLoading: false,
+      courseSlug: "",
+      link: this.$route.fullPath,
     };
   },
   // Set the link variable to the path of the current page.
   beforeMount() {
-    this.link = this.$route.path;
-    for (var quiz of settings.quizzes) {
-      this.quizzes.push({ id: quiz.id, name: quiz.name, });
-    }
+    this.courseSlug = this.$route.params.courseid;
+    this.isLoading = true;
+    this.getQuizzes();
+  },
+  methods: {
+    getQuizzes() {
+      var currentCourse = this.$store.state.user.courses.find(
+        x => x.course_slug === this.courseSlug
+      );
+      if (currentCourse) {
+        this.courseId = currentCourse.course_id;
+        util
+          .getQuizzes(this.courseId)
+          .then(response => {
+            this.quizzes = response.data;
+            this.isLoading = false;
+          })
+          .catch(err => {
+            console.log(err);
+            this.loadingText = strings.connection_error;
+          });
+      }
+    },
   },
 };
 </script>
+
+<style>
+.quiz-card ul {
+  list-style: none;
+  padding-left: 0;
+}
+
+.quiz-card ul .li-title {
+  font-weight: bold;
+}
+</style>
